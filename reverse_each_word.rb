4865eb04aff01a97af03dc4db9c9d25b3785b3e6
James Kim <jamesgki.kim67@gmail.com>
2017-08-17 05:06:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word(string)
   words = string.split
   reversed =
     words.collect do |word|
-      reverse = word.split("").reverse!
-      reverse.join("")
+      word.reverse
     end
   reversed.join(" ")
 end
